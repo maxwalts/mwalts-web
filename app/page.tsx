8523e12ba982719c58cc6f883910ad1c48f26f42
Max Walts <mwalts@umich.edu>
2023-07-19 03:08:41 -0500

    mobile fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ import { TwitterLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
   return (
-    <main className=" p-24 lg:p-20">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 lg:gap-4 gap-8 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="  h-full ">
+
+      <div className="z-10 w-full max-w-5xl items-center justify-between font lg:flex ">
+        {/* dark:from-black dark:via-black */}
+        <div className="lg:pl-24 lg:pt-24 fixed bottom-0 left-0 flex h-48 lg:gap-4 gap-8 w-full items-end justify-center bg-gradient-to-t from-background via-background  lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="flex place-items-center gap-2 pb-7 lg:pointer-events-auto lg:p-0"
             href="https://twitter.com/mwalts0"
@@ -32,6 +34,11 @@ export default function Home() {
 
       <InlineWidget
         url="https://calendly.com/max-walts/consulting-call"
+        styles={{
+          height: '1000px',
+          width: '100%',
+          // overflow: 'hidden',
+        }}
       />
     </main>
   )
