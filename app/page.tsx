ad6a9b3b963d1bddad150c6eaa1542202a985582
Max Walts <mwalts@umich.edu>
2023-07-19 03:52:40 -0500

    optimize styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,20 @@ import { TwitterLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
   return (
-    <main className="  h-full ">
+    <main className="">
 
-      <div className="z-10 w-full max-w-5xl items-center justify-between font lg:flex ">
-        {/* dark:from-black dark:via-black */}
-        <div className="lg:pl-24 lg:pt-24 fixed bottom-0 left-0 flex h-48 lg:gap-4 gap-8 w-full items-end justify-center bg-gradient-to-t from-background via-background  lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex ">
+        <div className="lg:pl-24 lg:pt-12 pb-7 lg:pb-0 fixed bottom-0 left-0 flex h-48 lg:gap-4 gap-8 w-full items-end justify-center bg-gradient-to-t from-background via-background lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="flex place-items-center gap-2 pb-7 lg:pointer-events-auto lg:p-0"
+            className=" lg:pointer-events-auto "
             href="https://twitter.com/mwalts0"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <TwitterLogoIcon href='' className="lg:w-6 lg:h-6 h-10 w-10 hover:text-blue-600" />
+            <TwitterLogoIcon className="lg:w-6 lg:h-6 h-10 w-10 hover:text-blue-600" />
           </a>
           <a
-            className="flex place-items-center gap-2 pb-7 lg:pointer-events-auto lg:p-0"
+            className="lg:pointer-events-auto "
             href="https://github.com/maxwalts"
             target="_blank"
             rel="noopener noreferrer"
@@ -30,14 +29,13 @@ export default function Home() {
           </a>
 
         </div>
+
       </div>
 
       <InlineWidget
         url="https://calendly.com/max-walts/consulting-call"
         styles={{
-          height: '1000px',
-          width: '100%',
-          // overflow: 'hidden',
+          height: '90vh',
         }}
       />
     </main>
